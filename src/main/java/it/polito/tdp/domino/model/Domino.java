dfec9a18f97fdf88cecc25bf9716b9102ad51fb1
genna <genna@LAPTOP-HDR1FFT9>
2021-04-07 11:36:55 +0200

    domino migliorabile

---
+++ src/main/java/it/polito/tdp/domino/model/Domino.java
@@ -0,0 +1,61 @@
+package it.polito.tdp.domino.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Domino {
+	public void sequenza() {
+		List<Integer> parziale= new LinkedList<>();
+		List<String> controllo= new LinkedList<>();
+		this.creaSequenza(parziale, 0, controllo);
+	}
+	
+	private void creaSequenza(List<Integer> parziale, int livello, List<String> controllo) {
+		if(parziale.size()==26) {
+			System.out.println(parziale);
+			
+		}
+		else {
+			
+			for(int i=1; i<=6; i++) {
+				int n=0;
+				for(int j=1; j<livello; j++) {
+					   if(parziale.get(j).equals(i))
+						 n++;
+					
+				}
+				if(livello==0) {
+					List<Integer> nuovaParziale= new LinkedList<>(parziale);
+					nuovaParziale.add(i);
+					creaSequenza(nuovaParziale, livello+1, controllo);
+					
+				}
+				if(n<5) {
+					
+					if(parziale.size()%2==0 && parziale.get(livello-1)==i) {
+					   List<String> nuovoControllo= new LinkedList<>(controllo);
+					   nuovoControllo.add(""+parziale.get(livello-1)+parziale.get(livello-2));
+					   nuovoControllo.add(""+parziale.get(livello-2)+parziale.get(livello-1));
+					   
+					   List<Integer> nuovaParziale= new LinkedList<>(parziale);
+					   nuovaParziale.add(i);
+					   creaSequenza(nuovaParziale, livello+1, nuovoControllo);
+					   
+					 }
+					if(parziale.size()%2!=0 && parziale.get(livello-1)!=i && !controllo.contains(""+parziale.get(livello-1)+i)) {
+					   List<Integer> nuovaParziale= new LinkedList<>(parziale);
+					   nuovaParziale.add(i);
+					   creaSequenza(nuovaParziale, livello+1, controllo);
+					
+					}
+				}
+				
+				
+			}
+			
+			
+		}
+		
+	}
+
+}
---
+++ src/main/java/it/polito/tdp/domino/model/TestDomino.java
@@ -0,0 +1,12 @@
+package it.polito.tdp.domino.model;
+
+public class TestDomino {
+
+	public static void main(String[] args) {
+		Domino prova= new Domino();
+		prova.sequenza();
+		
+	}
+
+	
+}
